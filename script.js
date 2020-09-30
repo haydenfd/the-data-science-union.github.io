c8903c27f6169b58ebd2ce3744abca6fa0915b53
Sophie Yun <sophieyun@Slothophie.lan>
2020-09-29 22:29:35 -0700

    prevent mobile view

--- script.js
+++ script.js
@@ -91,3 +91,22 @@ view_4.onclick = open_4;
 close_4.onclick = hide_4;
 view_5.onclick = open_5;
 close_5.onclick = hide_5;
+
+function phone() { // getter runs when you ask for the property
+    var md = new MobileDetect(window.navigator.userAgent);
+    return md.mobile() ? true : false;
+}
+
+$(document).ready(function() {
+
+    if (phone()) { // asking for subj.phone will detect phone
+        $("#loading div").css("width", "80%");
+        $("#loading div").html('hahahah.It seems that you are using a touchscreen device or a phone. Please use a laptop or desktop instead to view the page content.<br><br>Sorry about the inconvenience!');
+    } else {
+        $(window).on("load", function(){
+            setTimeout(function() {
+            $("#loading").fadeOut("slow");
+            }, 1000);
+        }); 
+    }
+});
