1ccdb2fc0f389690884cd510c87b455762cdae4e
haydenfd <haydenfds@gmail.com>
2022-03-21 07:24:26 -0700

    Deleted everything basically lol

--- client.js
+++
@@ -1,9 +0,0 @@
-// Form submission validation and emailing
-function submitForm(name, email, msg) {
-    if (isValidEmail()) {
-        email.send({
-            Host: "smtp.datascienceunion.com",
-            Username: ""
-        })
-    }
-}
